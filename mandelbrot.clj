682e826312f0cdd624f5d4938cec818db6c11421
Vishvajit Singh <vishvajitsingh@gmail.com>
2010-10-18 01:38:17 -0400

    calculate and draw the Mandelbrot set

--- mandelbrot.clj
+++ mandelbrot.clj
@@ -3,7 +3,8 @@
  '(javax.swing JPanel JFrame))
 
 (use 'clojure.contrib.complex-numbers
-     'clojure.contrib.generic.arithmetic)
+     'clojure.contrib.generic.arithmetic
+     'clojure.contrib.generic.math-functions)
 
 (defn make-panel [w h render]
   (doto (proxy [JPanel] []
@@ -15,3 +16,29 @@
     (.add (apply make-panel panel-args))
     .pack
     .show))
+
+; Returns the number of iterations for |z| to exceed 2
+; or nil if it never does.
+(defn num-mandelbrot-iterations [#^complex c max-iter]
+  (loop [z (complex 0.0 0.0) num-iter 0]
+    (if (> num-iter max-iter)
+      nil
+      (if (> (abs z) 2.0)
+	num-iter
+	(recur (+ (* z z) c) (inc num-iter))))))
+
+(defn mandelbrot []
+  (make-frame 768 512
+	      (fn [#^Graphics g]
+		(let [dot (fn [x y c]
+			    (.setColor g c)
+			    (.drawLine g x y x y))]
+		  (doseq [x (range 768) y (range 512)]
+		    (let [max-iter 30
+			  inv-max-iter (int (/ 255.0 max-iter))
+			  num-iter (num-mandelbrot-iterations (complex (+ (* (/ x 768.0) 3.0) -2.0)
+								       (+ (* (/ y 512.0) 2.0) -1.0))
+							      max-iter)]
+		      (dot x y (if (nil? num-iter)
+				 (Color. 0 0 0)
+				 (Color. (* num-iter inv-max-iter) 0 0)))))))))
